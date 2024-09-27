581ecc3b475bf4ad45365b2d9e25f4dfa0ca2253
zergula321 <max.kalinin.2001@gmail.com>
2024-09-27 19:09:32 +0300

    add base components

---
+++ src/components/Creature.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "Creature"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/PlayerPanel.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "PlayerPanel"
+}
+</script>
+
+<style scoped>
+
+</style>
