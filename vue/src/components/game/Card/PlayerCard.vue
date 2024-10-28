a1b8f637210e6ca727a1cf17b6a16961d5d0f3f3
IlyaPlotnikov <ilya-plotnikov@mail.ru>
2024-10-28 15:02:13 +0300

    add card component and game logic files

---
+++ vue/src/components/game/Card/PlayerCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div
+        class="card"
+    >
+        <p class="card__text">{{cardStruct.name}}</p>
+        <p class="card__text">цена: {{cardStruct.cost}}</p>
+        <p class="card__text">атака: {{cardStruct.attack}}</p>
+        <p class="card__text">защита: {{cardStruct.defence}}</p>
+    </div>
+</template>
+
+<script>
+import { Cards } from "@/gameLogic/cards"
+
+export default {
+    name: "PlayerCard",
+    props: ["cardId"],
+    computed: {
+        cardStruct() {
+            return Cards[this.cardId]
+        },
+    },
+}
+</script>
+
+<style lang="less">
+  @import "./styles/style";
+</style>
---
+++ vue/src/gameLogic/cards.js
@@ -0,0 +1,28 @@
+export const Cards = [
+    {
+        name: "Илюха еретик",
+        attack: "5",
+        defence: "2",
+        cost: "4",
+    },
+    {
+        name: "Максим аноним",
+        attack: "3",
+        defence: "3",
+        cost: "3",
+    },
+    {
+        name: "Алсу панда",
+        attack: "7",
+        defence: "3",
+        cost: "4",
+    },
+    {
+        name: "Ирыжик",
+        attack: "6",
+        defence: "6",
+        cost: "5",
+    },
+]
+
+export const MaxCardId = Cards.length - 1
