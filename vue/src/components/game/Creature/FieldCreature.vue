67699030e7ea9cec26d43c6933a1764717423c3d
IlyaPlotnikov <ilya-plotnikov@mail.ru>
2024-10-29 22:42:08 +0300

    add creature component

---
+++ vue/src/components/game/Creature/FieldCreature.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="creature">
+        <p class="creature__text">{{name}}</p>
+        <p class="creature__text">{{attack}} - {{defence}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FieldCreature",
+    props: ["attack", "defence", "name"]
+}
+</script>
+
+<style lang="less">
+  @import "./styles/style";
+</style>
