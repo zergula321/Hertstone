b73e1d860cb50485a9702227af3ae1906ac8cb81
zergula321 <max.kalinin.2001@gmail.com>
2024-10-31 20:51:15 +0300

    add field component

---
+++ vue/src/components/Field/GameField.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="field">
+      <FieldCreature
+        v-for="(creature, index) in creatures"
+        :name="creature.name"
+        :attack="creature.attack"
+        :defence="creature.defence"
+        v-bind:key="index"
+      />
+  </div>
+</template>
+
+<script>
+import FieldCreature from "@/components/game/Creature/FieldCreature.vue";
+
+export default {
+    name: "GameField",
+    components: {FieldCreature},
+    props: ["creatures"]
+}
+</script>
+
+<style lang="less">
+  @import "./styles/style";
+</style>
